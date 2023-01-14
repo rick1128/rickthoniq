6dd40c864464ec477bc7c64b04511760d1c5d685
روجر <104784913+rogerpq@users.noreply.github.com>
2023-01-14 20:46:59 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# ها وردة جاي تخمـط الملف كتابة - @I_0_P @ZQ_LO
+
+# Don't use quotes( " and " ) - [اختياري]
+
+APP_ID= ""
+API_HASH=
+STRING_SESSION= ""
+TG_BOT_TOKEN= ""
+TG_BOT_USERNAME= ""
+ENV= "ANYTHING"
+TZ= "Asia/Baghdad"
+DATABASE_URL= ""
+
+# [اجباري]
